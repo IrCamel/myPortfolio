29bdb7ba8a4fb0762074d66a1f09094f88045e12
IrCamel <dario03juve@gmail.com>
2024-09-02 16:57:57 +0200

    update style and social

--- myPortfolio/src/app/homepage/homepage.component.ts
+++ myPortfolio/src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -6,12 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
-  private currentIndex = 0;
 
   ngOnInit() {
     setTimeout(() => {
       const section = document.querySelector('.hero-section') as HTMLElement;
       section.classList.add('visible');
+      this.checkScroll(); // Controlla lo scroll al caricamento della pagina
     }, 100); // Delay per evitare flash di contenuto non stilizzato
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  private checkScroll(): void {
+    const elements = document.querySelectorAll('.text-animate') as NodeListOf<HTMLElement>;
+
+    elements.forEach((element) => {
+      const rect = element.getBoundingClientRect();
+
+      // Elemento visibile se almeno 0.5% è visibile nella viewport
+      if (rect.top <= window.innerHeight * 0.995 && rect.bottom >= window.innerHeight * 0.005) {
+        element.classList.add('visible');
+        element.classList.remove('invisible');
+      } else if (rect.bottom < window.innerHeight * 0.005 || rect.top > window.innerHeight * 0.995) {
+        element.classList.remove('visible');
+        element.classList.add('invisible');
+      }
+    });
   }
 }
